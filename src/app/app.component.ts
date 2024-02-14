c33d5f8ba26d0f3261834c39050cad1daeda331e
Amir Awak <amir.kian@awak.ch>
2024-02-14 09:30:40 +0100

    add primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [ButtonModule, RouterModule],
   selector: 'angular-primeng-nx-example-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-primeng-nx-example';
+
+  isClicked = false;
+
+  clickMe():void {
+    this.isClicked = !this.isClicked;
+  }
 }
