87b28e31bfe0cad211c92a026290b3b3e155b61e
Amir Awak <amir.kian@awak.ch>
2024-02-23 14:23:22 +0100

    add test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,40 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { BadgeModule } from 'primeng/badge';
 import { ButtonModule } from 'primeng/button';
+import { TabMenuModule } from 'primeng/tabmenu';
 
 @Component({
   standalone: true,
-  imports: [ButtonModule, RouterModule],
+  imports: [ButtonModule, TabMenuModule, RouterModule, BadgeModule, NgIf],
   selector: 'angular-primeng-nx-example-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-  title = 'angular-primeng-nx-example';
+export class AppComponent implements OnInit {
+  items = signal<MenuItem[] | undefined>([
+    { label: 'Home' },
+    { label: 'Calendar', icon: 'pi pi-fw pi-calendar' },
+    { label: 'Edit', icon: 'pi pi-fw pi-pencil' },
+    { label: 'Documentation', icon: 'pi pi-fw pi-file' },
+    { label: 'Settings', icon: 'pi pi-fw pi-cog' }
+  ]);
 
-  isClicked = false;
+  activeItem: MenuItem | undefined;
 
-  clickMe():void {
-    this.isClicked = !this.isClicked;
+  ngOnInit() {
+    const items = this.items();
+    if (items) this.activeItem = items[0];
+  }
+
+  myclick() {
+    console.log('clicked');
+
+    const items = this.items();
+    if (items) items[0].badge = 'hi';
+    this.items.set(items);
   }
 }
